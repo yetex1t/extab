30dbd29a6cf9fdc1bf001fac51d528013076632b
yetex1t <yetex1t@outlook.com>
2024-08-10 19:15:04 +0800

    refactor: optimize the search function code

--- js/search.js
+++ js/search.js
@@ -1,36 +1,24 @@
 window.onload = function () {
-    var searchInput = document.getElementById("searchtxt");
-    var searchButton = document.getElementById("searchbtn");
-
-    // 按 Tab 键聚焦搜索框
-    document.onkeydown = function (event) {
-        var ev = event || window.event;
-        if (ev.key == "Tab") {
-            searchInput.focus();
-            return false;
-        }
-    };
+    // 获取搜索输入框和搜索按钮
+    const searchInput = document.getElementById("searchtxt");
+    const searchButton = document.getElementById("searchbtn");
 
     // 按 Enter 键开始搜索
-    searchInput.onkeydown = function (event) {
-        var ev = event || window.event;
-        if (ev.key == "Enter") {
-            if (searchInput.value != "") {
-                window.location.href = `https://www.baidu.com/s?ie=utf-8&wd=${encodeURIComponent(
-                    searchInput.value
-                )}`;
-                searchInput.value = "";
-            }
+    searchInput.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            performSearch();
         }
-    };
+    });
 
     // 点击图标进行搜索
-    searchButton.onclick = function () {
-        if (searchInput.value != "") {
-            window.location.href = `https://www.baidu.com/s?ie=utf-8&wd=${encodeURIComponent(
-                searchInput.value
-            )}`;
-            searchInput.value = "";
+    searchButton.addEventListener('click', performSearch);
+
+    // 执行搜索操作
+    function performSearch() {
+        const query = searchInput.value.trim();
+        if (query !== '') {
+            window.location.href = `https://www.baidu.com/s?ie=utf-8&wd=${encodeURIComponent(query)}`;
+            searchInput.value = '';
         }
-    };
+    }
 };
